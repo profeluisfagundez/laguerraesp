1c6a00e71474e2aa62f7dd598af1d22fba389d66
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-12-19 12:31:29 -0300

    se trabaja con el proyecto

--- app/controller/FrontController.php
+++ app/controller/FrontController.php
@@ -1,7 +1,15 @@
 <?php
+require_once("LoginController.php"); 
+session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $dato = $_POST['type'];
+    $type = $_POST['type'];
+    $nombre = $_POST['nombre'];
+    $contra = $_POST['contra'];
+    if ($type === 'login') {
+        $login = new LoginController();
+        $login->login($nombre, $contra);
+    }
 }
 
 ?>
